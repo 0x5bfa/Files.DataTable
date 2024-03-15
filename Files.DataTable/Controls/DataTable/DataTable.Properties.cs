8a7da9a09110e0545ed03cff0f0a20a8566c1d06
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-03-15 12:55:38 +0900

    Removed unnecessary code

---
+++ Files.DataTable/Controls/DataTable/DataTable.Properties.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using CommunityToolkit.WinUI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.Foundation;
+
+namespace Files.DataTable;
+
+public partial class DataTable : Panel
+{
+	/// <summary>
+	/// Gets or sets the amount of space to place between columns within the table.
+	/// </summary>
+	public double ColumnSpacing
+	{
+		get => (double)GetValue(ColumnSpacingProperty);
+		set => SetValue(ColumnSpacingProperty, value);
+	}
+
+	/// <summary>
+	/// Gets the <see cref="ColumnSpacing"/> <see cref="DependencyProperty"/>.
+	/// </summary>
+	public static readonly DependencyProperty ColumnSpacingProperty =
+		DependencyProperty.Register(
+			nameof(ColumnSpacing),
+			typeof(double),
+			typeof(DataTable),
+			new PropertyMetadata(0d));
+}
