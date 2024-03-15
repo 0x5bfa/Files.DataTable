3fded6fa3e8b2e09757fc684a2c8fd0622f0dff2
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-03-15 17:44:20 +0900

    Update

---
+++ Files.DataTable/SortDirection.cs
@@ -0,0 +1,18 @@
+﻿namespace Files.DataTable
+{
+	/// <summary>
+	/// SortDirection is used instead of the CommunityToolkit equivalent because it is tied to the model
+	/// </summary>
+	public enum SortDirection : byte
+	{
+		/// <summary>
+		/// Sort in ascending order.
+		/// </summary>
+		Ascending = 0,
+
+		/// <summary>
+		/// Sort in descending order.
+		/// </summary>
+		Descending = 1
+	}
+}
