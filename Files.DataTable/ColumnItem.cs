7b6d8ed41166a61862b734f151c0ac2ad6ccd47b
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-03-17 03:00:41 +0900

    Added item

---
+++ Files.DataTable/ColumnItem.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.Graphics.Imaging;
+
+namespace Files.DataTable
+{
+	public class ColumnItem
+	{
+		public string? Name { get; set; }
+
+		public GridLength DesiredWidth { get; set; }
+
+		public int MinWidth { get; set; } = 60;
+
+		public bool CanResize { get; set; } = true;
+	}
+}
